a4f4dd382e8af0f731bbab6259ef04dc75addcc6
Masha003 <colta.masha@gmail.com>
2025-06-03 12:17:34 +0300

    Add signout route

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/signin', authController.signin);
-// router.post('/signout', identifier, authController.signout);
+router.post('/signout', authController.signout);
 
 // router.patch(
 // 	'/send-verification-code',
